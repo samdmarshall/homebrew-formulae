830365f5cf6e86855cfbcdf1de14be956c812811
Samantha Marshall <me@samdmarshall.com>
2015-09-12 15:40:24 -0700

    updating shasum

--- html2pdf.rb
+++ html2pdf.rb
@@ -3,7 +3,7 @@ class Html2pdf < Formula
   homepage "https://github.com/samdmarshall/html2pdf"
   head "https://github.com/samdmarshall/html2pdf.git"
   url "https://github.com/samdmarshall/html2pdf/releases/download/1.0/html2pdf.zip"
-  sha256 "ff5f5515d896f3a60a18529d43a9010c89eb2a17"
+  sha256 "ff6f64d0767e34c4d4bf029532d80a6a27f718aea587604d558dcfe7bfe40f92"
   
   def install
     bin.install Dir[libexec/"bin/*"]
