9fb36a0d012f807d47ea7c9ab724fdc1f665eed1
Samantha Marshall <me@samdmarshall.com>
2015-09-12 12:46:33 -0700

    adding formulae and readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# Sam's homebrew formulae
+
+## AOSD
+
+Apple Open Source Downloader
+
+* Uses: [HEAD](https://github.com/samdmarshall/AOS-Downloader)
+
+---
+
+## xcparse
+
+* Uses: [HEAD](https://github.com/samdmarshall/xcparse)
+
+
+---
+
+## Mai
+
+* Uses: [HEAD](https://github.com/samdmarshall/Mai)
---
+++ xcparse.rb
@@ -0,0 +1,15 @@
+class xcparse < Formula
+  desc "Library to interact with the Xcode Build System"
+  homepage "https://github.com/samdmarshall/xcparse"
+  head "https://github.com/samdmarshall/xcparse.git"
+
+  depends_on :python if MacOS.version <= :snow_leopard
+  
+  def install
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+    system "python", *Language::Python.setup_install_args(libexec)
+
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+  end
+end
