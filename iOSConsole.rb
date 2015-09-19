7951f59767469c380998e8adaa5a3e3948a19eaf
Samantha Marshall <me@samdmarshall.com>
2015-09-19 09:38:07 -0400

    adding iOSConsole formula

---
+++ iOSConsole.rb
@@ -0,0 +1,11 @@
+class IOSConsole < Formula
+  desc "CLI tool to interact with iOS Devices"
+  homepage "https://github.com/samdmarshall/SDMMobileDevice"
+  head "https://github.com/samdmarshall/SDMMobileDevice.git"
+  url "https://github.com/samdmarshall/SDMMobileDevice/releases/download/1.1/iOSConsole.zip"
+  sha256 "ab18c419ee40999128220438130946943925703f498c72cbdbd3fd43f1adfd09"
+  
+  def install
+    bin.install Dir[libexec/"bin/*"]
+  end
+end
