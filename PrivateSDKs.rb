a7f8ed2928d852a5efcf9d11f6263b8521642317
Samantha Marshall <me@samdmarshall.com>
2015-09-19 10:20:36 -0400

    testing formula

---
+++ PrivateSDKs.rb
@@ -0,0 +1,10 @@
+class PrivateSDKs < Formula
+  desc "Supplimental SDKs for building Apple's open source packages"
+  homepage "https://github.com/samdmarshall/OSXPrivateSDK"
+  head "https://github.com/samdmarshall/OSXPrivateSDK.git"
+  
+  def install
+    system "mkdir -p ~/Library/Developer/AdditionalSDKS/"
+    mv "*.sparse.sdk" "~/Library/Developer/AdditionalSDKS/"
+  end
+end
