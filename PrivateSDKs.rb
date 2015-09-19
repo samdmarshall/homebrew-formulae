3f2143d98a18b7f9614ace0cdd51d19ebb74afad
Samantha Marshall <me@samdmarshall.com>
2015-09-19 13:31:25 -0400

    formula seems to work now

--- PrivateSDKs.rb
+++ PrivateSDKs.rb
@@ -4,7 +4,26 @@ class Privatesdks < Formula
   head "https://github.com/samdmarshall/OSXPrivateSDK.git"
   
   def install
-    system "mkdir", "-p", "~/Library/Developer/AdditionalSDKS/"
-    mv "*.sparse.sdk", "~/Library/Developer/AdditionalSDKS/"
+    additional_sdk_dir = `eval echo "~$USER/Library/Developer/AdditionalSDKs/"`
+    additional_sdk_dir.delete!("\n")
+    unless File.exist?(additional_sdk_dir)
+      puts "Creating directory to store additional SDKs..."
+      Dir.mkdir(additional_sdk_dir)
+    end
+    for file in Dir["*.sparse.sdk"]
+      file.delete!("\n")
+      sdk_dir = additional_sdk_dir+"/"+file
+      sdk_dir.delete!("\n")
+      if File.exist?(sdk_dir)
+        puts "Removing old versions of '"+file+"'..."
+        FileUtils.rm_r sdk_dir
+      end
+      puts "Installing '"+file+"'..."
+      temp_path = "#{buildpath}"+"/"+file
+      temp_path.delete!("\n")
+      FileUtils.mv temp_path, additional_sdk_dir
+      puts "* To use '"+file+"', set the ADDITIONAL_SDKS build setting to '${USER_LIBRARY_DIR}/Developer/AdditionalSDKs/"+file+"'\n"
+    end
+    puts "Installed SDKs to '"+additional_sdk_dir+"'!"
   end
 end
