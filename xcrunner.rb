5e18d1105836aec7630834b68ff3aaa963be7be9
Samantha Marshall <me@samdmarshall.com>
2015-10-01 20:02:24 -0400

    typo

--- xcrunner.rb
+++ xcrunner.rb
@@ -4,6 +4,7 @@ class Xcrunner < Formula
   head "https://github.com/samdmarshall/xcrunner.git"
   url "https://github.com/samdmarshall/xcrunner/releases/download/v1.0/xcrunner.zip"
   sha256 "b8e1f78b70f473a97047ac20459e9e6e44853f13b34a029b53ddfefff1c30669"
+  version "1.0"
   
   def install
     bin.install "xcrunner"
