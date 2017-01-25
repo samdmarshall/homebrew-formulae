b700d20fa0ef826cba66a11d96e15236b831b067
Samantha Marshall <me@samdmarshall.com>
2017-01-25 15:15:21 -0500

    updating secure-env to install with a bottle

--- secure-env.rb
+++ secure-env.rb
@@ -4,13 +4,17 @@ class SecureEnv < Formula
 
   # v0.1
   ##
-  url "https://github.com/samdmarshall/secure-env/archive/v0.1.zip"
-  version "0.1"
-  sha256 "552429de38eaf1326ee6b869ed56605e3b51c6b706fcac85d56138d3368c4af9"
+  url "https://github.com/samdmarshall/secure-env/archive/v0.1.tar.gz"
+  sha256 "3d1344b01b60e643ad9fabcbf47d223e8c6b65678495359fb1e512d51971f2f8"
 
   # --HEAD
   ##
   head "https://github.com/samdmarshall/secure-env.git"
+
+  bottle do
+    root_url "https://pewpewthespells.com/bottles"
+    sha256 "5fb1fbaeccca56fbdcf0e93c9b3c13bda2f70d579292e91a02fe92b9401850d0" => :sierra
+  end
 
   # dependencies
   ##
@@ -19,5 +23,9 @@ class SecureEnv < Formula
   def install
     system "nim compile --out:secure-env --app:console --passL:\"-framework Foundation -framework Security\" secureEnv.nim"
     bin.install "secure-env"
+  end
+
+  test do
+    echo "yr good!"
   end
 end
