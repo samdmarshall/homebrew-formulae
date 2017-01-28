bb23be7200166a5dbbfe7170f0b3baf01ed6aaea
Samantha Marshall <me@samdmarshall.com>
2017-01-28 08:52:15 -0500

    adding c2nim

---
+++ c2nim.rb
@@ -0,0 +1,21 @@
+class C2nim < Formula
+  desc "c2nim"
+  homepage "https://github.com/nim-lang/c2nim"
+
+  # --HEAD
+  ##
+  head "git@github.com:nim-lang/c2nim.git"
+
+  # dependencies
+  ##
+  depends_on "nim"
+
+  def install
+    system "nimble", "build"
+    bin.install "c2nim"
+  end
+
+  test do
+    echo "yr good!"
+  end
+end
