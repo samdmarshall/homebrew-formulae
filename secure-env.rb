85b9330aab30dd10850a996fe40f4d7f62036bc6
Samantha Marshall <me@samdmarshall.com>
2017-01-29 08:22:38 -0500

    bumping version number

--- secure-env.rb
+++ secure-env.rb
@@ -4,24 +4,19 @@ class SecureEnv < Formula
 
   # v0.1
   ##
-  url "https://github.com/samdmarshall/secure-env/archive/v0.1.tar.gz"
-  sha256 "3d1344b01b60e643ad9fabcbf47d223e8c6b65678495359fb1e512d51971f2f8"
+  url "https://github.com/samdmarshall/secure-env/archive/v0.2.tar.gz"
+  sha256 "885b27cc6cca04b60354983b58ee4cea07e4b4c6e2d66ef86718603c9a61b5f0"
 
   # --HEAD
   ##
   head "https://github.com/samdmarshall/secure-env.git"
-
-  bottle do
-    root_url "https://pewpewthespells.com/bottles"
-    sha256 "5fb1fbaeccca56fbdcf0e93c9b3c13bda2f70d579292e91a02fe92b9401850d0" => :sierra
-  end
 
   # dependencies
   ##
   depends_on "nim"
 
   def install
-    system "nim compile --out:secure-env --app:console --passL:\"-framework Foundation -framework Security\" secureEnv.nim"
+    system "nim", "compile", "secureEnv.nim"
     bin.install "secure-env"
   end
 
