49d0ef55a118073d4b1a400bc6196c006101c2c5
Samantha Marshall <me@samdmarshall.com>
2017-01-31 20:53:09 -0500

    updating battery-level

--- battery-level.rb
+++ battery-level.rb
@@ -2,10 +2,11 @@ class BatteryLevel < Formula
   desc "securely populate environment variables"
   homepage "https://github.com/samdmarshall/battery-level"
 
-  # v0.1
+  url "https://github.com/samdmarshall/battery-level/archive/v0.2.tar.gz"
+  sha256 "d05337129dfb1cc19d773e19b3017855575b2d6e4c7aae00fa57a8597ed217c8"
   ##
-  url "https://github.com/samdmarshall/battery-level/archive/v0.1.tar.gz"
-  sha256 "feb6475369671670dd4a1c833d6cbe7b4c6b95662851420588b7ffc4f7a0ab90"
+  #url "https://github.com/samdmarshall/battery-level/archive/v0.1.tar.gz"
+  #sha256 "feb6475369671670dd4a1c833d6cbe7b4c6b95662851420588b7ffc4f7a0ab90"
 
   # --HEAD
   ##
