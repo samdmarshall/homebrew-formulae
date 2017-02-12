cec48d45e6e2e6ef92cc355cfa0476a10b0b9b11
Samantha Marshall <me@samdmarshall.com>
2017-02-12 12:09:35 -0500

    adding dagger

---
+++ dagger.rb
@@ -0,0 +1,22 @@
+class Dagger < Formula
+  desc "Binary Translator to LLVM IR"
+  homepage "https://github.com/repzret/dagger"
+
+  head "https://github.com/repzret/dagger.git"
+
+  depends_on "cmake" => :build
+  depends_on "subversion" => :build
+
+  def install
+    mkdir "build" do
+      args = std_cmake_args
+      args << "-DCMAKE_OSX_SYSROOT=/" unless MacOS::Xcode.installed?
+      args << "-DLLVM_ENABLE_LIBCXX=ON"
+      args << ".."
+      system "cmake", "-G", "Unix Makefiles", *args
+      system "make"
+      bin.install "bin/llvm-dec"
+      lib.install "lib/libDYN.dylib"
+    end
+  end
+end
