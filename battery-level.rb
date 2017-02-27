9973e258e2a2552f2799c65c0c6d2d27c2d6e72d
Samantha Marshall <me@samdmarshall.com>
2017-02-27 12:08:53 -0500

    updating battery-level

--- battery-level.rb
+++ battery-level.rb
@@ -2,8 +2,10 @@ class BatteryLevel < Formula
   desc "easy access to current macbook battery levels on the command line"
   homepage "https://github.com/samdmarshall/battery-level"
 
-  url "https://github.com/samdmarshall/battery-level/archive/v0.3.tar.gz"
-  sha256 "3fe3aec759172ea208681f6cab1d5c05d51954e35c29277cc67c03c61b30f7d8"
+  url "https://github.com/samdmarshall/battery-level/archive/v0.4.tar.gz"
+  sha256 "d7620af5a9a6f136ce949aaf92f21a06e9b4729045c21cd64564c0462c740f5c"
+  # url "https://github.com/samdmarshall/battery-level/archive/v0.3.tar.gz"
+  # sha256 "3fe3aec759172ea208681f6cab1d5c05d51954e35c29277cc67c03c61b30f7d8"
   ##
   #url "https://github.com/samdmarshall/battery-level/archive/v0.2.tar.gz"
   #sha256 "d05337129dfb1cc19d773e19b3017855575b2d6e4c7aae00fa57a8597ed217c8"
