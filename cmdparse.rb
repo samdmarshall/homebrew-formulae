a9306c98436c20b8af25bccb045585804e7ac133
Samantha Marshall <me@samdmarshall.com>
2017-03-02 13:35:24 -0500

    adding influx logging

---
+++ cmdparse.rb
@@ -0,0 +1,21 @@
+class cmdparse < Formula
+  desc "posting data from the command line into influxdb"
+  homepage "https://github.com/samdmarshall/cmd-parse"
+
+  # --HEAD
+  ##
+  head "https://github.com/samdmarshall/cmd-parse.git"
+
+  # dependencies
+  ##
+  depends_on "nim"
+
+  def install
+    system "nim", "compile", "cmdparse.nim"
+    bin.install "cmdparse"
+  end
+
+  test do
+    echo "yr good!"
+  end
+end
