a25f299428cfc522986a15d3abbf11e0131a5cc7
Samantha Marshall <me@samdmarshall.com>
2017-03-13 10:48:18 -0400

    adding cs-tools to formulae

---
+++ cs-tools.rb
@@ -0,0 +1,17 @@
+class CsTools < Formula
+  desc "some general codesigning related tools"
+  homepage "https://github.com/samdmarshall/cs-tools"
+
+  # head
+  ##
+  head "https://github.com/samdmarshall/cs-tools.git"
+
+  def install
+    system "make"
+    bin.install "build/find-identity"
+    bin.install "build/guardian"
+    bin.install "build/quarantine"
+    bin.install "build/vaccine"
+    bin.install "build/signature"
+  end
+end
