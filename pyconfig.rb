73338e715762219ef9811965520ede95c06cbbb4
Samantha Marshall <me@samdmarshall.com>
2017-04-05 11:04:30 -0400

    pyconfig -> 1.1.3

--- pyconfig.rb
+++ pyconfig.rb
@@ -23,9 +23,12 @@ class Pyconfig < Formula
 #	url "https://github.com/samdmarshall/pyconfig/archive/v1.1.1.tar.gz"
 #	sha256 "333996ade82b1ba02c7b62fbb6803fcde98ef3ceadf3cfce49fb3e808119e09d"
 
-	version "1.1.2"
-	url "https://github.com/samdmarshall/pyconfig/archive/v1.1.2.tar.gz"
-	sha256 "f629a3069d2f5ab6731eccbf9e490d80cf33a463041c0a2703be961b1d1be2e6"
+	# version "1.1.2"
+	# url "https://github.com/samdmarshall/pyconfig/archive/v1.1.2.tar.gz"
+	# sha256 "f629a3069d2f5ab6731eccbf9e490d80cf33a463041c0a2703be961b1d1be2e6"
+
+  url "https://github.com/samdmarshall/pyconfig/archive/v1.1.3.tar.gz"
+  sha256 "b7d5e7f0e6579d627dba330e5e8f24815be7a3e215c26220a343f9d7531ccf4b"
 
 	depends_on :python if MacOS.version <= :snow_leopard
   depends_on "pyparsing" => :python
