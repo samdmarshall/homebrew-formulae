d3079703d1d74f8cf9d96a7fbbf02f28676753c2
Samantha Marshall <me@samdmarshall.com>
2017-04-12 08:32:42 -0400

    Adding bugmatic formula

---
+++ bugmatic.rb
@@ -0,0 +1,10 @@
+class Bugmatic < Formula
+  desc "Offline-searchable bug tracker with support for locally enqueueing bugs and then synching back to Github issues."
+  homepage "https://github.com/uliwitness/bugmatic"
+  head "https://github.com/uliwitness/bugmatic.git"
+  
+  def install
+      system "make"
+      bin.install "bugmatic"
+    end
+end
