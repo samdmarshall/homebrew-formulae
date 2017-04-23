1017c0e27c43e76424d5dd88bcee161f1ae427df
Samantha Marshall <me@samdmarshall.com>
2017-04-22 21:18:26 -0400

    Updating when to v0.3

--- when.rb
+++ when.rb
@@ -3,8 +3,10 @@ class When < Formula
   homepage "https://github.com/samdmarshall/when"
   head "https://github.com/samdmarshall/when.git"
 
-  url "https://github.com/samdmarshall/when/archive/v0.2.tar.gz"
-  sha256 "1cdb4ee3fd8d02ed446c5dea5de448a8e36e939566752ca543d9b8e0e07d00e5"
+  url "https://github.com/samdmarshall/when/archive/v0.3.tar.gz"
+  sha256 "4af2801909e6f5979e25326a93a2da0fad62ce3232d048eb41ee94d5d19d59c0"
+  #url "https://github.com/samdmarshall/when/archive/v0.2.tar.gz"
+  #sha256 "1cdb4ee3fd8d02ed446c5dea5de448a8e36e939566752ca543d9b8e0e07d00e5"
   #url "https://github.com/samdmarshall/when/archive/v0.1.tar.gz"
   #sha256 "fd1ee1c034bd2bbdeeef1264522e6d2aefd0f4d079e294f9cf21458cf39a91e6"
   
