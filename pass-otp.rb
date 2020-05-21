a4bca345d20c169ce642956e1d4856c842567023
Samantha Demi <hello@pewpewthespells.com>
2020-05-21 15:41:21 -0400

    updating pass extension formulae

--- pass-otp.rb
+++ pass-otp.rb
@@ -2,19 +2,16 @@ class PassOtp < Formula
   desc "pass-otp"
   homepage "https://github.com/tadfisher/pass-otp"
 
-  # head
-  ##
+  url "https://github.com/tadfisher/pass-otp/releases/download/v1.2.0/pass-otp-1.2.0.tar.gz"
+  sha256 "5720a649267a240a4f7ba5a6445193481070049c1d08ba38b00d20fc551c3a67"
+
   head "https://github.com/tadfisher/pass-otp.git"
 
-  def install
-    bin.install "otp.bash"
-    install_path = "#{HOMEBREW_PREFIX}/lib/password-store/extensions"
-    mkdir_p install_path
-    ln_s "#{bin}/otp.bash", install_path
-    man1.install "pass-otp.1"
-  end
+  depends_on "shellcheck" => :test
+  depends_on "oath-toolkit"
+  depends_on "pass"
 
-  def post_install
-    
+  def install
+    system "make", "PREFIX=#{prefix}", "BASHCOMPDIR=#{bash_completion}", "install"
   end
 end
