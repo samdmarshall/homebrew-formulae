823bde33ba28baa88ad0b2bf3934108224a1b155
Samantha Demi <hello@pewpewthespells.com>
2020-05-21 15:45:07 -0400

    updating getmail formula to not use python2

--- getmail.rb
+++ getmail.rb
@@ -5,6 +5,8 @@ class Getmail < Formula
   url "http://pyropus.ca/software/getmail/old-versions/getmail-5.14.tar.gz"
   sha256 "f3a99fe74564237d12ca8d4582e113c067c9205b5ab640f72b4e8447606a99c1"
 
+  depends_on "python"
+
   def install
     libexec.install %w[getmail getmail_fetch getmail_maildir getmail_mbox]
     inreplace Dir[libexec/"*"], %r{^#!/usr/bin/env python$}, "#!/usr/bin/python"
